b50246a5403d825199c08b533c15f0d2541a8c3a
Aleksandr Ermolov <90044699+AleksandrErmolov@users.noreply.github.com>
2022-01-27 22:33:21 +0300

    add easy Drag and Drop

--- src/component/DrugAndDropEasy.jsx
+++ src/component/DrugAndDropEasy.jsx
@@ -12,12 +12,63 @@ export default function DrugAndDropEasy() {
     ])
 
 
+    const [currentCard, setCurrentCard] = useState(null)
+
+
+    function dragStartHandler(e, card) { 
+        console.log('drag', card)
+        setCurrentCard(card)
+    }
+
+
+    function dragLeaveHandler(e) {
+        e.target.style.background = 'white'
+    }
+
+
+    function dragEndHandler(e) {
+       
+    }
+
+    function dragOverHandler(e) {
+        e.preventDefault()
+       e.target.style.background = 'lightgray'
+    }
+
+    function dropHandler(e, card) {
+        e.preventDefault()
+        setCardList(cardList.map(c => { 
+            if (c.id === card.id) { 
+                return {...c, order:currentCard.order}
+            }
+            if (c.id === currentCard.id) {
+                return { ...c, order: card.order }
+            }
+            return c
+        }))
+        e.target.style.background = 'white'
+
+    }
+
+    const sortCards = (a,b) => { 
+        if (a.order > b.order) {
+            return 1
+        } else { 
+            return -1
+        }
+    }
+
     return (
         <div className="cards">
-            {cardList.map(card =>
+            {cardList.sort(sortCards).map(card =>
                 <div
+                    key={card.id}
                     draggable
-
+                    onDragStart={(e) => dragStartHandler(e, card)}
+                    onDragLeave={(e) => dragLeaveHandler(e)}
+                    onDragEnd={(e) => dragEndHandler(e) }
+                    onDragOver={(e) => dragOverHandler(e)}
+                    onDrop={(e) => dropHandler(e, card)}
                     className="card">
                     {card.text}
                 </div>
